9bebb4bc5291f112e30c4ca0e25f3789281831fa
tali <atalyad@gmail.com>
2012-07-04 15:06:02 +0200

    Fix unicode characters issue and change the display to be shorter.

--- weather.py
+++ weather.py
@@ -1,60 +1,53 @@
 __author__ = 'AP'
+from urllib import quote
 import urllib2
 from xml.dom import minidom
 
 WEATHER_URL = 'http://www.google.com/ig/api?weather='
+
 
 def get_weather_for_location(location):
 
     try:
 
         try:
-            dom = minidom.parse(urllib2.urlopen(WEATHER_URL+location))
+            res = urllib2.urlopen(WEATHER_URL+ quote(location))
+            dom = minidom.parseString(unicode(res.readlines()[0],  errors='ignore'))
         except :
             return 'Could not read the weather data for your location... it has funky characters in it....'
 
         if dom.getElementsByTagName('problem_cause'):
             return 'is that a real place?... can\'t find it... :( '
 
-        ans = '\n Weather forecast for %s \n\n' % location
+        ans = ''
         for node in dom.getElementsByTagName('forecast_information'):
              for n in node.getElementsByTagName('forecast_date'):
                 ans += 'Forecast date: %s, ' % n.getAttribute('data')
              for n in node.getElementsByTagName('city'):
                  ans += (' Taken at %s' % n.getAttribute('data'))
-             for n in node.getElementsByTagName('current_date_time'):
-                 ans += ' on %s' % n.getAttribute('data')
-             ans += '\n\n'
+             ans += '\n'
 
-        for node in dom.getElementsByTagName('current_conditions'):
-            ans += '*Current conditions*: \n'
-            for n in node.getElementsByTagName('condition'):
-                ans += 'Condition: %s\n' % n.getAttribute('data')
-            for n in node.getElementsByTagName('temp_c'):
-                ans += 'Temps: %s c\n' % n.getAttribute('data')
-            for n in node.getElementsByTagName('humidity'):
-                ans += '%s\n' % n.getAttribute('data')
-            for n in node.getElementsByTagName('wind_condition'):
-                ans += '%s\n\n' % n.getAttribute('data')
-
-
-        ans += '*Forecast conditions*: \n\n'
+        i = 0
         for node in dom.getElementsByTagName('forecast_conditions'):
-            for n in node.getElementsByTagName('day_of_week'):
-                ans += 'Day of week: %s\n' % n.getAttribute('data')
+            if not i:
+                ans += 'Today: '
+            else:
+                for n in node.getElementsByTagName('day_of_week'):
+                    ans += n.getAttribute('data') + ': '
             for n in node.getElementsByTagName('condition'):
-                ans += '%s\n' % n.getAttribute('data')
+                ans += n.getAttribute('data') + ' '
             for n in node.getElementsByTagName('low'):
                 low = int(n.getAttribute('data'))
                 if low is not None:
-                    ans += 'Low: %s c\n' % str(round((low - 32) / (9.0/5.0))) #convert to celsius
+                    ans += str(round((low - 32) / (9.0/5.0))) #convert to celsius
             for n in node.getElementsByTagName('high'):
                 high = int(n.getAttribute('data'))
                 if high is not None:
-                    ans += 'high %s c\n\n' % str(round((high - 32) / (9.0/5.0))) #convert to celsius
+                    ans += ' - %s' % str(round((high - 32) / (9.0/5.0))) + u'\u2103, ' #convert to celsius
+            i+= 1
 
 
-        return ans
+        return ans.rstrip(' ,')
 
     except Exception,e:
         return 'There was an error: %s' % str(e)
