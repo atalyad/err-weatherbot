70b2f988f085e44f02c21c287b7887c17a6ab4b3
Oded Lazar <oded@senexx.com>
2015-10-20 19:03:33 +0300

    fixed a few issues - - added support for python 2 and 3 - added plugin configuration: OpenWeatherMap api key and units

---
+++ consts.py
@@ -0,0 +1,6 @@
+# -*- coding: utf8 -*-
+API_KEY_CONFIG_NAME = 'api_key'
+UNITS_CONFIG_NAME = 'units'
+LOCATION_CONFIG_NAME = 'location'
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?'
+DATE_FMT = '%Y-%m-%d %H:%M:%S'
